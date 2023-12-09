c86d709b65f9a66be29079269be237772e5e8312
= <divyanshdh@gmail.com>
2023-12-09 11:50:47 +0530

    Update commit

--- src/Component/WorkData.js
+++ src/Component/WorkData.js
@@ -2,6 +2,8 @@ import image3 from "../Assests/ProjImage/Image3.png"
 import image4 from "../Assests/ProjImage/Image4.png"
 import image9 from "../Assests/ProjImage/Image9.png"
 import image5 from "../Assests/ProjImage/Image5-1.png"
+import image2 from "../Assests/ProjImage/Image2.png"
+import image6 from "../Assests/ProjImage/Image6.png"
 
 const ProjectCardData = [
     {
@@ -15,7 +17,7 @@ const ProjectCardData = [
         imgsrc : image3,
         title : "OutLay Manager",
         description : "I developed a web application dedicated to expense management, leveraging the Chart.js library for creating dynamic and interactive data visualizations. The primary objective was to optimize the user experience through a user-centric design approach.  By incorporating Chart.js into your application, you can present financial data in a more digestible and insightful manner. ",
-        view : "View.com",
+        view : "https://outlay-tracker-ft.netlify.app/",
         source : "https://github.com/git-divyansh/Outlay-manager"
     },
     {
@@ -26,14 +28,14 @@ const ProjectCardData = [
         source : "https://github.com/git-divyansh/Epics-proj"
     },
     {
-        imgsrc : image5,
+        imgsrc : image6,
         title : "URLock",
         description : "Circuit-level gateways offer a wider variety of possible rules than other firewalls along with its proxy service, so it is more versatile in this regard. For example, it can block specific URLs from being accessed, whereas a packet filter would need the IP address of the website, which may not be unique since many large companies such as Google use multiple servers. This code is made to run the file on Kali Linux but yes you can run it on any other os as well. Just you need to change the 'host_path'. ",
         view : "View.com",
         source : "https://github.com/git-divyansh/Circuit-Level-Firewall"
     },
     {
-        imgsrc : image5,
+        imgsrc : image2,
         title : "Port Scanner",
         description : "A port scanner is an essential utility in the domain of network security and administration. It plays a pivotal role in systematically probing and assessing the status of open ports on a computer or network to discern potential security vulnerabilities or services actively running on those ports. This tool operates by sending a series of network requests to specific port numbers associated with a target system or network device. It aids in the proactive identification of potential vulnerabilities.",
         view : "View.com",
