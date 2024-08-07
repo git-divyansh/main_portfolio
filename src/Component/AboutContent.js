c85b84b9fb3ff76c654aad8d18f9d66636b37ed4
= <divyanshdh@gmail.com>
2024-08-07 23:21:06 +0530

    Update build

--- src/Component/AboutContent.js
+++ src/Component/AboutContent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import "./AboutContent.css";
 import { Link } from 'react-router-dom';
 import image2 from "../Assests/image5.jpg";
